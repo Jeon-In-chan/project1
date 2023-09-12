4e17ebba2bc72223271472d9fab973e9fb8f131c
shy <tjgkdud054@gmail.com>
2023-09-12 17:41:14 +0900

    shoppingBasket.jsp 간략하게 구현

---
+++ src/main/java/com/team4/project1/domain/BasketVO.java
@@ -0,0 +1,20 @@
+package com.team4.project1.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BasketVO {
+
+	private String b_no;
+	private String u_id;
+	private String p_no;
+	private int quantity;
+	private int price;
+	private Date b_regDate;
+	private boolean image;
+	
+
+
+}
